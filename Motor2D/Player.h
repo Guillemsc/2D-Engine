a28bdc6598c5ee72e87304d7e2836fd86818ab0b
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2017-01-31 01:27:10 +0100

    Folders changed

--- Motor2D/Player.h
+++ Motor2D/Player.h
@@ -9,14 +9,28 @@ public:
 	Player();
 	~Player();
 
+	// Load animations
 	bool LoadEntity();
+
+	// Start variables
 	bool Start();
+
+	// PreUpdate
 	bool PreUpdate();
+
+	// Update
 	bool Update(float dt);
+
+	// Draw and change animations
 	bool Draw();
+
+	// Post Update
 	bool PostUpdate();
+
+	// CleanUp
 	bool CleanUp();
 
+	// On Collision
 	void OnColl(PhysBody* bodyA, PhysBody* bodyB, b2Fixture* fixtureA, b2Fixture* fixtureB);
 };
 
